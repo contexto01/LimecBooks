77e5460c3f881f662e460d715d85e6d95a1b4551
Daniel Barrera <daniel.barrera02121@hotmail.com>
2024-09-07 11:22:08 -0500

    Cors fixed

--- Server/src/app.ts
+++ Server/src/app.ts
@@ -15,7 +15,14 @@ const mongoUri = process.env.MONGODB_URI
 
 app.use(morgan('dev'))
 app.use(express.json())
-app.use(cors())
+app.use(
+  cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Authorization'
+  })
+)
 
 app.use('/', router)
 
@@ -28,46 +35,6 @@ if (mongoUri) {
   console.error('Please set the MONGODB_URI environment variable', mongoUri)
   process.exit(1)
 }
-
-// app.get('/', (_req: Request, res: Response) => {
-//   res.send(db)
-// })
-
-// app.post('/api/books', (req: Request, res: Response) => {
-//   const book = req.body
-//   db.push({
-//     id: crypto.randomUUID(),
-//     title: book.title,
-//     author: book.author,
-//     description: book.description
-//   })
-//   res.send(db)
-// })
-
-// app.get('/api/books', (_req: Request, res: Response) => {
-//   res.send(db)
-// })
-
-// app.get('/api/books/:id', (req: Request, res: Response) => {
-//   const id = req.params.id
-//   const book = db.find((b) => b.id === id)
-//   res.send(book)
-// })
-
-// app.put('/api/books/:id', (req: Request, res: Response) => {
-//   const id = req.params.id
-//   const book = req.body
-//   const index = db.findIndex((b) => b.id === id)
-//   db[index] = book
-//   res.send(db)
-// })
-
-// app.delete('/api/books/:id', (req: Request, res: Response) => {
-//   const id = req.params.id
-//   const index = db.findIndex((b) => b.id === id)
-//   db.splice(index, 1)
-//   res.send(db)
-// })
 
 app.listen(port, () => {
   console.log('Server is running on port http://localhost:3000')
